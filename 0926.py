982209baf51ddfba377f471f61e72acec943fdf1
wade990047 <wade990047@gmail.com>
2022-09-26 17:15:46 +0800

    0926

--- 0926.py
+++ 0926.py
@@ -1,4 +1,15 @@
-kg = float(input("體重:"))
-m = float(input("身高:"))
-bmi = kg/(m*m)
-print(bmi)
+# kg = float(input("體重:"))
+# m = float(input("身高:"))
+# bmi = kg/(m*m)
+# print(bmi)
+
+import requests
+
+url = "https://www.nkust.edu.tw/p/403-1000-1363-{}.php?Lang=zh-tw"
+for i in range(1,51) :
+  u = url.format(i)
+  #r = requests.get(u)
+  if i%2 == 0:
+    print(u)
+
+    
