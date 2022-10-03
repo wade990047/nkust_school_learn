4cda42cfcb5b68a906c89db7fb621e327c1d74a4
wade990047 <wade990047@gmail.com>
2022-10-03 17:24:30 +0800

    1003

--- 1003-2.py
+++ 1003-2.py
@@ -1,6 +1,10 @@
 data = list()
-for i in range(1,10,2):
-    data.append(i)
-    i += 1
-print(i)
-print(data)
+total = 0
+n = input("人數:")
+for i in range(int(n)):
+    score = int(input("輸入分數:"))
+    data.append(score)
+print("最高分:" , max(data))
+print("最低分:" , min(data))
+print("總平均:" , sum(data)/len(data))
+print("總分" , sum(data))
