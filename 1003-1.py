282d6afb8733c14238dd9fed6b93b01438d459e6
wade990047 <wade990047@gmail.com>
2022-10-03 17:14:16 +0800

    1003

--- 1003-1.py
+++ 1003-1.py
@@ -1,7 +1,7 @@
 scores = []
 while True:
-    score = input("輸入分數(輸入e退出):")
-    if score == "e":
+    score = input("輸入分數(輸入end退出):")
+    if score == "end":
         print("資料內容:" + str(scores))
         print("資料長度:" + str(len(scores)))
         print("資料型態:" + str(type(scores)))
