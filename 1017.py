e9cf57be043849beb34422e2547679516ac0d43f
wade990047 <wade990047@gmail.com>
2022-10-17 17:14:41 +0800

    1017

--- 1017.py
+++ 1017.py
@@ -1,3 +1,4 @@
+'''
 chi = int(input("chi="))
 while chi>=0 or chi>100:
     eng = int(input("eng="))
@@ -6,3 +7,13 @@ while chi>=0 or chi>100:
     print(total,avg)
     chi = int(input("chi="))
 print("end")
+'''
+data = []
+score = int(input("score="))
+while score >= 0:
+    data.append(score)
+    score = int(input("score="))
+print("最高分:",max(data))
+print("最低分:",min(data))
+print("總分:",sum(data))
+print("平均:",round(sum(data)/len(data),2))
