ac484dd6c0dfd02f49fd44e18c2cb6021007ae28
wade990047 <wade990047@gmail.com>
2022-11-05 03:32:58 +0800

    hw

--- poker_hw.py
+++ poker_hw.py
@@ -9,25 +9,16 @@ def scores(a,score):
   return score
 def say(a,b):
   print("\n你的手牌是:",a)
-  print("你的分數是:",b)
+  print("你的分數是:",b,"\n")
+def setup():
+  card_face = ["Diamond"]*13 + ["Spade"]*13 + ["Heart"] * 13 + ["Club"] * 13
+  card_value = [i for i in range(1, 14)] * 4
+  deck = [item for item in zip(card_face, card_value)]
+  random.shuffle(deck)
+  return deck
 
-
-#建立一個card_face串列，用來放所有牌面的花色，以下總共52個項目（每個花色各13個）
-card_face = ["Diamond"]*13 + ["Spade"]*13 + ["Heart"] * 13 + ["Club"] * 13
-
-#建立一個card_value串列，用來放所有牌面的點數，以下總共52個項目（每個花色各13個值，分別是1~13）
-card_value = [i for i in range(1, 14)] * 4
-
-#把前面兩個串列組合成一個串列deck，用來放所有的撲克牌內容（依照順序放），內容分別是('Diamond', 1), ('Diamond', 2), ..., ('Club', 13)
-deck = [item for item in zip(card_face, card_value)]
-
-#把deck串列中的牌順序打亂
-random.shuffle(deck)
-
-#建立一個用來存放玩家手上的牌的串列player_cards
-player_cards = list()
-
-#把deck串列中的最後1張牌pop出來，放到player_cards串列中
+deck = setup()
+player_cards = []
 player_cards.append(deck.pop())
 
 scoreout = 0
@@ -43,18 +34,28 @@ while answer != "n" and replay != "n":
   i = i + 1
   num = player_cards[i][1]
   scoreout = scores(num,scoreout)
-  if scoreout > 10.5:
-    print("\n你的分數爆掉了")
-    say(player_cards,scoreout)
-    print()
+  if scoreout == 10.5:
+    print("您贏了! 恭喜您剛好獲得10.5點")
     replay = input("是否再玩一次?(Y/N):").lower()
     if replay == "y":
-      card_face = ["Diamond"]*13 + ["Spade"]*13 + ["Heart"] * 13 + ["Club"] * 13
-      card_value = [i for i in range(1, 14)] * 4
-      deck = [item for item in zip(card_face, card_value)]
-      random.shuffle(deck)
+      deck = setup()
       player_cards = []
-
+      scoreout = 0
+      i = 0
+      player_cards.append(deck.pop())
+      num = player_cards[i][1]
+      scoreout = scores(num,scoreout)
+      say(player_cards,scoreout)
+      answer = input("請問你要補牌嗎?(Y/N):").lower()
+    else:
+      break
+  elif scoreout > 10.5:
+    print("\n你的分數爆掉了")
+    say(player_cards,scoreout)
+    replay = input("是否再玩一次?(Y/N):").lower()
+    if replay == "y":
+      deck = setup()
+      player_cards = []
       scoreout = 0
       i = 0
       player_cards.append(deck.pop())
