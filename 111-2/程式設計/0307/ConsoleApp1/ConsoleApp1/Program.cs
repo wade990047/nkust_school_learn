7cff0312031c8676bdb85a419d04c7b376de4008
wade990047 <wade990047@gmail.com>
2023-03-07 11:58:11 +0800

    0307

---
+++ 111-2/程式設計/0307/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            double h, w;
+            Console.Write("身高(公尺):");
+            h = Convert.ToDouble(Console.ReadLine());
+            Console.Write("體重(公斤):");
+            w = Convert.ToDouble(Console.ReadLine());
+            Console.Write("BMI:");
+            Console.WriteLine(Math.Round(w/Math.Pow(h,2)));
+        }
+    }
+}
