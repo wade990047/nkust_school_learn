219d9cb2ae5d3d1abab2eab1b6b3c093931f7320
wade990047 <wade990047@gmail.com>
2023-05-11 15:24:36 +0800

    0511

--- 111-2/電商網站/header.php
+++ 111-2/電商網站/header.php
@@ -3,17 +3,17 @@
 ?>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="#">選單</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/ecwww111-2/">Home</a>
+                    <a class="nav-link active" aria-current="page" href="index.php">首頁</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="https://www.ccet.nkust.edu.tw">不分系</a>
+                    <a class="nav-link" href="https://ccet.nkust.edu.tw/">不分系</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="rate.php">匯率換算</a>
@@ -26,6 +26,7 @@
                         <li><a class="dropdown-item" href="bmi.php">BMI計算</a></li>
                         <li><a class="dropdown-item" href="lotto.php">樂透預測</a></li>
                         <li><a class="dropdown-item" href="bodyinfo.php">健康管理</a></li>
+                        <li><a class="dropdown-item" href="js-example/index.html">JS練習</a></li>
                     </ul>
                 <?php
                     if ($_SESSION["user"] != "administrator") {
